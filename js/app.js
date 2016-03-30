326f6f1b73ac403828ed363a7e4d0ea9cfef1fa0
Mac <Mavericks@Mavericks-3.local>
2016-03-30 16:45:22 -0700

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var $ = require('jquery');
+var Contact = require('./models/contact');
 
 $(document).ready(function(){
   var Router = require('./routes/router');
@@ -8,4 +9,20 @@ $(document).ready(function(){
     var href = $(this).attr('href').substr(1);
     Router.navigate(href, {trigger:true});
   });
+  $("#container").on("click", "#button", function (e){
+  	e.preventDefault();
+  	var contact = new Contact({
+  });
+
+  	contact.set({
+    "fname": $("#fname").val(),
+    "lname": $("#lname").val(),
+    "email": $("#email").val(),
+    "phone": $("#phone").val(),
+    "city": $("#city").val(),
+    "state": $("#state").val(),
+    "picture": $("#picture").val(),
+  });
+  contact.save();
+  })
 });
--- js/routes/router.js
+++ js/routes/router.js
@@ -1,7 +1,11 @@
 var Backbone = require('../lib/backbone-parse/backbone-parse');
-var indexTemplate = require('../../templates/index.html');
+var FormTemplate = require('../../templates/form.html');
+var ContactList = require("../../templates/contact-list.html")
+var SingleView = require ("../../templates/single-view.html")
 var Contact = require('../models/contact');
+var Contacts = require("../collections/contacts")
 var $ = require('jquery');
+require('font-awesome/css/font-awesome.min.css');
 
 var Router = Backbone.Router.extend({
   initialize: function () {
@@ -9,10 +13,16 @@ var Router = Backbone.Router.extend({
   },
   routes: {
     '': 'index',
-    'contact/:contactId': 'contact'
+    'contact/:contactId': 'contact',
+    "new": "new"
   },
   index: function () {
-    $("#container").html(indexTemplate());
+    Contacts.fetch({
+    success: function (contacts){
+      var data = { contacts: contacts.toJSON() };
+      $("#container").html(ContactList(data));
+    }
+  })
   }
 });
 
@@ -22,12 +32,17 @@ router.on('route:contact', function (contactId){
   var contact = new Contact({
     objectId: contactId
   });
-
   contact.fetch({
-    success:function(contact){
-      $("#container").html(contact.get('first_name'));
+    success: function (contact){
+      console.log(contact);
+      $("#container").html(SingleView(contact));
     }
   })
+})
+
+router.on("route:new", function(){
+  $("#container").html(FormTemplate);
+  
 })
 
 module.exports = router;
